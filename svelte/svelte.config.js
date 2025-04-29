98597775476481756e4e803899de84a159db04f9
Dawid <dawidkotlin@gmail.com>
2025-04-29 08:23:00 +0200

    deploy

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -3,6 +3,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+	},
 	kit: { adapter: adapter() }
 };
 
